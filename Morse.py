0a54c5dca51052eea9eaf2023e0bd5c9794c0263
AKS <60446729+AngKS@users.noreply.github.com>
2021-11-02 08:29:47 +0800

    First Commit

---
+++ Morse.py
@@ -0,0 +1,15 @@
+
+'''
+Name: Ang Kah Shin
+Class: DAAA/FT/2B/04
+Admin: P2004176
+'''
+
+# morse code dictionary
+lookUpTable = {}
+
+# Morse Code Class
+
+class Morse:
+    def __init__(self):
+        self.morse_code = 
---
+++ main.py
@@ -0,0 +1,8 @@
+
+'''
+Name: Ang Kah Shin
+Class: DAAA/FT/2B/04
+Admin: P2004176
+'''
+
+# Writing program 
