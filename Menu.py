5195e575b877b881c31c218b10c6178ae4b7785f
AKS <60446729+AngKS@users.noreply.github.com>
2021-11-18 22:02:55 +0800

    modify menu class; start working on main program loop

--- Menu.py
+++ Menu.py
@@ -1,26 +1,35 @@
 
 class Menu:
-    def __init__(self, question, order, callback):
+    '''Menu class takes in 3 parameters:
+    1. question: The question to be asked to the user
+    2. order: Boolean value to determine whether the menu should be ordered or not
+    3. callback: The function to be called when the user enters a valid input
+
+    Functions Available:
+    1. insert(items): Inserts items into the menu
+    2. __str__(): Prints the menu
+    '''
+
+    def __init__(self, question, order):
         self.qn = question
         self.ord = order
         self.__items = []
         self.length = 0
-        self.callback = callback
 
     def insert(self, items):
         for item in items:
             self.length += 1
             self.__items.append(item)
-        
+
     def __checkInput(self, userInput):
         # Check user input to see if it exists in the list
         # If it does, return the index of the item
         if userInput.isnumeric():
             userInput = int(userInput)
             if userInput >= 1 and userInput <= self.length:
-                return userInput - 1
-        print("Invalid input")
-        return self.__str__()
+                return str(userInput)
+        print("** <Invalid input> **")
+        return self.show()
 
     def __str__(self):
         print("\n" + self.qn)
@@ -31,21 +40,21 @@ class Menu:
             for item in self.__items:
                 print("-", item)
 
+        return ""
+
+    def show(self):
+        print("\n" + self.qn)
+        if self.ord:
+            for i in range(self.length):
+                print(str(i+1) + ". " + self.__items[i])
+        else:
+            for item in self.__items:
+                print("-", item)
+
         userInput = self.__checkInput(input("Enter choice: "))
-        return self.callback(userInput)
+        return userInput
+
 
 def testFunction(userInput):
     print("You entered: ", userInput + 1)
     return ""
-
-newMenu = Menu("What do you want to do?", True, testFunction)
-
-newMenu.insert([
-    "Hang out",
-    "Watch movies",
-    "Watch the sunset"
-])
-
-print(newMenu)
-    
-    
--- main.py
+++ main.py
@@ -4,15 +4,45 @@ Name: Ang Kah Shin
 Class: DAAA/FT/2B/04
 Admin: P2004176
 '''
-# Writing program 
+# Writing program
 
 from Morse import Morse
 from Menu import Menu
 
+
+def mainBanner():
+    banner = '''
+*******************************************
+* ST1507 DSAA: MorseCode Message Analyzer *
+*-----------------------------------------*
+*                                         *
+* - Done by: Ang Kah Shin (P2004176)      *
+* - Class: DAAA/FT/2B/04                  *
+******************************************* 
+    '''
+    return banner
+
+
 def main():
-    print("")
+    '''The main Program loop'''
+    # program variables:
+    pMode = 'h'
 
+    print(mainBanner())
+    # Main menu selection
+    mainMenu = Menu("Please select your choice (1, 2, 3, 4)", True )
+    # add options into mainMenu
+    mainMenu.insert([
+        "Change Printing Mode",
+        "Convert Morse Code to Text",
+        "Analyze Morse Code Message",
+        "Exit"
+    ])
+    # print mainMenu
+    userInput = mainMenu.show()
 
+    if userInput == 1:
+        print(f"Current printing mode is: {pMode}")
 
 if __name__ == "__main__":
     main()
